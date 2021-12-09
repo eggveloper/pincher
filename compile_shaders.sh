2b06baae4bc08e7ef9685c9a373fde2a2f46b1f9
eggveloper <94803635+eggveloper@users.noreply.github.com>
2021-12-09 18:21:45 +0200

    fix shader compile script

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -1,2 +1,2 @@
-external/glslc shaders/simple_shader.vert -o shaders/simple_shader.vert.spv
-external/glslc shaders/simple_shader.frag -o shaders/simple_shader.frag.spv
+glslc shaders/simple_shader.vert -o shaders/simple_shader.vert.spv
+glslc shaders/simple_shader.frag -o shaders/simple_shader.frag.spv
