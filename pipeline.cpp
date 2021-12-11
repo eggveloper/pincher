0a3e8ffc7d2ff4422f5513c426d395720c912619
eggveloper <94803635+eggveloper@users.noreply.github.com>
2021-12-11 13:05:48 +0200

    update pipeline to use new device headers

--- pipeline.cpp
+++ pipeline.cpp
@@ -5,8 +5,13 @@
 #include <stdexcept>
 
 namespace pincher {
-    Pipeline::Pipeline(const std::string& vertFilepath, const std::string& fragFilepath) {
-        createGraphicsPipeline(vertFilepath, fragFilepath);
+    Pipeline::Pipeline(
+        Device& device,
+        const std::string& vertFilepath,
+        const std::string& fragFilepath,
+        const PipelineConfigInfo& configInfo
+    ) : device{device} {
+        createGraphicsPipeline(vertFilepath, fragFilepath, configInfo);
     }
 
     std::vector<char> Pipeline::readFile(const std::string& filepath) {
@@ -27,12 +32,31 @@ namespace pincher {
     }
 
     void Pipeline::createGraphicsPipeline(
-        const std::string& vertFilepath, const std::string& fragFilepath
+        const std::string& vertFilepath,
+        const std::string& fragFilepath,
+        const PipelineConfigInfo& configInfo
     ) {
         auto vertCode = readFile(vertFilepath);
         auto fragCode = readFile(fragFilepath);
 
         std::cout << "[DEBUG][PINCHER][pipeline.cpp] Vertex Shader Code Size: " << vertCode.size() << std::endl;
         std::cout << "[DEBUG][PINCHER][pipeline.cpp] Fragment Shader Code Size: " << fragCode.size() << std::endl;
+    }
+
+    void Pipeline::createShaderModule(const std::vector<char>& code, VkShaderModule* shaderModule) {
+        VkShaderModuleCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+        createInfo.codeSize = code.size();
+        createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+
+        if (vkCreateShaderModule(device.device(), &createInfo, nullptr, shaderModule) != VK_SUCCESS) {
+            throw std::runtime_error("[ERROR][PINCHER][pipeline.cpp] Failed to create shader module.");
+        }
+    }
+
+    PipelineConfigInfo Pipeline::defaultPipelineConfigInfo(uint32_t width, uint32_t height) {
+        PipelineConfigInfo configInfo{};
+
+        return configInfo;
     }
 }
